4b9af6fb80c8a5bb1d48e55f67ec17833a19b56b
AlhanGarcia <agpm13@gmail.com>
2022-09-21 00:47:39 -0500

    bisection methods

--- tests/BisectionTests.java
+++ tests/BisectionTests.java
@@ -10,10 +10,11 @@ public class BisectionTests {
         // Function to test is x2 + x - 12, roots are -4 and 3
         // Initial guesses for negativeRoot are -5 and 0
         var negativeRoot = BisectionMethod.bisection(new double[] {1.0, 1.0, -12.0}, -5, 0);
-        // Initial guesses for positiveRoot are 0 and 5
+        // Initial guesses for positiveRoot are 0 and 5 
         var positiveRoot = BisectionMethod.bisection(new double[] {1.0, 1.0, -12.0}, 0, 5);
         assertEquals(-4, negativeRoot, 0.001);
         assertEquals(3, positiveRoot, 0.001);
+
     }
 
 
